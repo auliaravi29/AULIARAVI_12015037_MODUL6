2d87fd608ca7cf640de726541cb8c6ac3bcb9e5f
rifazhr <rahmadinaarifazhari@gmail.com>
2018-04-01 23:13:24 +0700

    modul6

---
+++ app/src/main/java/com/example/asus/auliaravi_1202150037_modul6/Homescreen/PagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.asus.auliaravi_1202150037_modul6.Homescreen;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.example.asus.auliaravi_1202150037_modul6.FragmentMyPost;
+import com.example.asus.auliaravi_1202150037_modul6.FragmentNewPost;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    private int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                return new FragmentNewPost();
+            case 1:
+                return new FragmentMyPost();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
